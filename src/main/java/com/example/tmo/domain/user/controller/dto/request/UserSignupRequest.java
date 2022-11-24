f34bdc1fae1bb4719481ee71007a7b1c601fe7b8
김민채 <103401813+min56@users.noreply.github.com>
2022-11-24 13:45:56 +0900

    Update UserSignupRequest.java

--- src/main/java/com/example/tmo/domain/user/controller/dto/request/UserSignupRequest.java
+++ src/main/java/com/example/tmo/domain/user/controller/dto/request/UserSignupRequest.java
@@ -22,7 +22,7 @@ public class UserSignupRequest {
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
-            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자로 작성해주세오.")
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자로 작성해주세요.")
 
     private String password;
 
