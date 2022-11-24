02a10b7e6fa4fc5698c096493b5cc912ab77bae1
min56 <minchaeg85@gmail.com>
2022-11-24 14:01:55 +0900

    ✨:: 이미지 업로드 구현

---
+++ src/main/java/com/example/tmo/global/image/s3/S3Facade.java
@@ -0,0 +1,59 @@
+package com.example.tmo.global.image.s3;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.tmo.global.exception.FileIsEmptyException;
+import com.example.tmo.global.exception.FileSaveFailedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class S3Facade {
+
+    private final S3Properties s3Properties;
+    private final AmazonS3Client amazonS3Client;
+
+    public String uploadImage(MultipartFile file) {
+
+        String fileName;
+
+        if(file.isEmpty()) {
+            throw FileIsEmptyException.EXCEPTION;
+        }
+
+        try {
+            fileName = saveImage(file);
+        } catch (Exception e) {
+            throw FileSaveFailedException.EXCEPTION;
+        }
+
+        return getFileUrl(fileName);
+    }
+
+    private ObjectMetadata getObjectMetadata(MultipartFile image) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(image.getSize());
+        objectMetadata.setContentType(image.getContentType());
+
+        return objectMetadata;
+    }
+
+    private String saveImage(MultipartFile file) throws IOException {
+        String fileName = s3Properties.getBucket() + "/" + UUID.randomUUID() + file.getOriginalFilename();
+
+        amazonS3Client.putObject(new PutObjectRequest(s3Properties.getBucket(), fileName,
+                file.getInputStream(), getObjectMetadata(file)));
+        return fileName;
+    }
+
+    public String getFileUrl(String fileName) {
+        return amazonS3Client.getUrl(s3Properties.getBucket(), fileName).toString();
+    }
+}
---
+++ src/main/java/com/example/tmo/global/image/s3/S3Properties.java
@@ -0,0 +1,14 @@
+package com.example.tmo.global.image.s3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "cloud.aws.s3")
+public class S3Properties {
+    private final String bucket;
+}
