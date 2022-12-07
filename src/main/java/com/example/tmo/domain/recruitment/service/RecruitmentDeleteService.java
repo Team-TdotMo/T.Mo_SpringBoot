0c793c42f69b40bd23e68bbd3b8b4b5957007a3e
min56 <minchaeg85@gmail.com>
2022-12-07 22:52:31 +0900

    💩 :: 게시글 삭제 구현

--- src/main/java/com/example/tmo/domain/recruitment/service/RecruitmentDeleteService.java
+++ src/main/java/com/example/tmo/domain/recruitment/service/RecruitmentDeleteService.java
@@ -1,8 +1,8 @@
 package com.example.tmo.domain.recruitment.service;
 
+import com.example.tmo.domain.image.domain.repository.RecruitmentImageRepository;
 import com.example.tmo.domain.recruitment.domain.Recruitment;
 import com.example.tmo.domain.recruitment.domain.repository.RecruitmentRepository;
-import com.example.tmo.domain.recruitment.exception.RecruitmentNotDeleteException;
 import com.example.tmo.domain.recruitment.facade.RecruitmentFacade;
 import com.example.tmo.domain.user.domain.User;
 import com.example.tmo.domain.user.facade.UserFacade;
@@ -10,11 +10,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+
 @RequiredArgsConstructor
 @Service
 public class RecruitmentDeleteService {
 
     private final RecruitmentRepository recruitmentRepository;
+    private final RecruitmentImageRepository recruitmentImageRepository;
     private final RecruitmentFacade recruitmentFacade;
     private final UserFacade userFacade;
 
@@ -23,11 +26,9 @@ public class RecruitmentDeleteService {
 
         User user = userFacade.getCurrentUser();
         Recruitment recruitment = recruitmentFacade.findByRecruitmentId(recruitmentId);
-
-        if(!user.equals(recruitment.getUser())) {
-            throw RecruitmentNotDeleteException.EXCEPTION;
-        }
+        recruitmentFacade.checkWriter(recruitment);
 
         recruitmentRepository.delete(recruitment);
+        recruitmentImageRepository.deleteAllByIdInBatch(Collections.singleton(recruitmentId));
     }
 }
